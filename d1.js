6a51885328af474e18d27d52cc1638167218c41a
ArniZenux <arnijoha@hi.is>
2020-01-26 09:53:31 +0000

    d1 completed

--- d1.js
+++ d1.js
@@ -1,5 +1,11 @@
 
 function myFall(){
 	var hello = "Hello d1.js";
-    document.getElementById("demo").innerHTML = hello;
+	var nr1 = Number(document.getElementById("nr1").value);
+	var nr2 = Number(document.getElementById("nr2").value);
+    
+    var sum = nr1 + nr2; 
+
+    document.getElementById("demo").innerHTML = sum;
+
 }
